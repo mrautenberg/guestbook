3afe3c81d274349f1171a3bc82f8d5ce887b1e22
mrautenberg <mrautenberg@outlook.com>
2020-09-28 16:14:27 +0200

    This is a new commit for what I originally planned to be amended

---
+++ app.js
@@ -0,0 +1,39 @@
+// DATE FNS LIBRARY
+
+// Variables
+const name = document.querySelector(".name");
+const message = document.querySelector(".message-input");
+const messageList = document.querySelector(".message-list");
+const submitBtn = document.querySelector(".submit-btn");
+
+// Functions
+const addMessage = (e) => {
+    e.preventDefault();
+
+    // Add datetime somehow!
+    const now = new Date();
+    const month = now.getMonth();
+    const day = now.getDate();
+    const hour = now.getHours();
+    const minutes = now.getMinutes();
+
+    // Input values from form fields
+    const input = `${name.value}: ${message.value} (${day}/${month} at ${hour}:${minutes}) `;
+
+    // creating a new LI
+    const newMessage = document.createElement("li");
+    newMessage.innerText = input;
+    newMessage.classList.add("new-message");
+
+    // Append new message to list of messages
+    messageList.appendChild(newMessage);
+
+    // Clear input fields
+    name.value = "";
+    message.value = "";
+
+    console.log(newMessage);
+};
+
+// Event Listeners
+submitBtn.addEventListener("click", addMessage);
